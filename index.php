daf992e3ba386edf4a1107771dc846979c6d2769
NPDelMundo <delmundonielpatrick@gmail.com>
2024-07-22 12:59:14 +0800

    CRUD UPDATE DONE

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary"  data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary" name="submit">Understood</button>
+                            <button type="submit" class="btn btn-primary" name="submit">Add User</button>
                         </div>
                     </form>
                 </div>
